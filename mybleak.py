3c04080c2849731fa547ec895ed86fb187d48756
Mihail589 <lyapkin12@list.ru>
2025-11-22 20:05:47 +0300

    11 тест

--- mybleak.py
+++ mybleak.py
@@ -14,8 +14,9 @@ class BleGatt(BaseBle):
         self._obj_manager_iface = "org.freedesktop.DBus.ObjectManager"
         self._adapter_iface = "org.bluez.Adapter1"
         self._device_iface = "org.bluez.Device1"
-        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-        self._dbussetting()
+        if not is_server:
+            dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+            self._dbussetting()
         self.address = address
         self.uuids = uuids
        
