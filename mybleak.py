510cf9f4021ac159ed720284c40fe7fe1b0960c8
Mihail589 <lyapkin12@list.ru>
2026-01-29 23:17:04 +0300

    12 тест

--- mybleak.py
+++ mybleak.py
@@ -216,7 +216,7 @@ class BleGatt(BaseBle):
         return True
     def recvall(self, size: int) -> bytes:
         return super().recvall(size)
-print(BleGatt().discover())
+print(BleGatt().discover(2))
 ble = BleGatt(uuids = DeviceUuids("12345678-1234-5678-1234-56798abcdef0", "12345678-1234-5678-1234-56798abcdef1", "12345678-1234-5678-1234-56798abcdef2"))
 ble.connect("B8:27:EB:06:5D:3D")
 print(ble.write(b'$M<\x00\x01\x01'))
